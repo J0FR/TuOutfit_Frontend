4f7a98e0fcbc8e04557468dff6490b2da62e097a
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2023-05-01 11:39:47 -0500

    Cambios faltantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutfitModule } from './outfit/outfit.module';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OutfitModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
