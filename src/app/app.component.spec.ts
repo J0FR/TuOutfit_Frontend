4eced043e738c7c6d03a7384a32d6ebb1167164a
Daniel Pedroza <d.pedroza@uniandes.edu.co>
2023-05-08 23:50:09 -0500

    Arreglo final

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,14 +4,13 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { OutfitListComponent } from './outfit/outfit-list/outfit-list.component';
 import { MarcaListComponent } from './marca/marca-list/marca-list.component';
-
 import { PrendaListComponent } from './prenda/prenda-list/prenda-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientModule],
-      declarations: [AppComponent, OutfitListComponent, MarcaListComponent],
+      declarations: [AppComponent, OutfitListComponent, MarcaListComponent, PrendaListComponent],
     }).compileComponents();
   });
 
---
+++ src/app/marca/marca-detail/marca-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component,Input, OnInit } from '@angular/core';
+import { Marca } from '../marca';
+
+@Component({
+  selector: 'app-marca-detail',
+  templateUrl: './marca-detail.component.html',
+  styleUrls: ['./marca-detail.component.css']
+})
+export class MarcaDetailComponent implements OnInit {
+
+  @Input() marcaDetail!: Marca;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
