45bd27150b74e2fbb03f584fbd25ea8736256108
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2023-05-29 14:14:36 -0500

    Routers Basicos Listos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,23 @@ import { PrendaModule } from './prenda/prenda.module';
 import { TiendafisicaModule } from './tiendafisica/tiendafisica.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ComentarioModule } from './comentario/comentario.module';
+import { RouterModule, Routes } from '@angular/router';
+import { OutfitListComponent } from './outfit/outfit-list/outfit-list.component';
+import { PrendaListComponent } from './prenda/prenda-list/prenda-list.component';
+import { MarcaListComponent } from './marca/marca-list/marca-list.component';
+import { TiendafisicaListComponent } from './tiendafisica/tiendafisica-list/tiendafisica-list.component';
+import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
+import { NotFoundComponent } from './NotFound/NotFound.component';
 
+const appRoute: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'Outfits', component: OutfitListComponent},
+  {path: 'Prendas', component: PrendaListComponent},
+  {path: 'Marcas', component: MarcaListComponent},
+  {path: 'Tiendas', component: TiendafisicaListComponent},
+  {path: '**', component: NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
@@ -25,7 +41,9 @@ import { ComentarioModule } from './comentario/comentario.module';
     TiendafisicaModule,
     UsuarioModule,
     ComentarioModule,
-    HttpClientModule
+    HttpClientModule,
+    HomeModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
