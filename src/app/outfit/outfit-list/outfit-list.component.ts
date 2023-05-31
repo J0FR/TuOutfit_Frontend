3413c1835429b7a122b33790b1737cc34446642d
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2023-05-30 23:56:56 -0500

    Filtros Outfits

--- src/app/outfit/outfit-list/outfit-list.component.ts
+++ src/app/outfit/outfit-list/outfit-list.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { OutfitService } from '../outfit.service';
 import { OutfitDetail } from '../outfitDetail';
 import { Router } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { outputAst } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-outfit-list',
@@ -24,6 +26,21 @@ export class OutfitListComponent implements OnInit {
     });
   }
 
+  selectedGender = 'Genero';
+	onSelectedFilterGender(value:string): void {
+		this.selectedGender = value;
+	}
+
+  selectedAge = 'Edad';
+	onSelectedFilterAge(value:string): void {
+		this.selectedAge = value;
+	}
+
+  selectedOccasion = 'Ocasion';
+	onSelectedFilterOcassion(value:string): void {
+		this.selectedOccasion = value;
+	}
+
   getNumberOfOutfits(): number {
     return this.outfits.length;
   }
@@ -31,6 +48,15 @@ export class OutfitListComponent implements OnInit {
   onSelected(outfit: OutfitDetail): void {
     this.router.navigate(['/Outfits', outfit.id]);
   }
+
+  // @Output()
+  // filterGenderListSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  // @Output()
+  // filterAgeListSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  // @Output()
+  // filterOcassionListSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnInit() {
     this.getOutfits();
