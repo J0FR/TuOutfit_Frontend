7d74123eb2748018f951226393ba5cdfffeafdec
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2023-06-01 16:45:14 -0500

    Avances | Funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'base-project';
+
+  isPanelOpen = false;
+
+  togglePanel() {
+    this.isPanelOpen = !this.isPanelOpen;
+  }
 }
