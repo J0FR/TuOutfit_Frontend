e6363a20ade4beece183e4a2c461975c94ac5a07
Álvaro A. Bacca <a.baccap@uniandes.edu.co>
2023-06-02 02:16:19 -0500

    Página de "Nosotros" y arreglos en Navbar

---
+++ src/app/nosotros/nosotros.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NosotrosComponent } from './nosotros.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [NosotrosComponent],
+  declarations: [NosotrosComponent]
+})
+export class NosotrosModule { }
