befb24d6d5ad9a1b1bb1fbbda45a3f758918e168
Luisfetoga2 <l.torresg@uniandes.edu.co>
2023-06-04 00:11:39 -0500

    prendas-outfits

--- src/app/prenda/prenda-detail/prenda-detail.component.ts
+++ src/app/prenda/prenda-detail/prenda-detail.component.ts
@@ -3,6 +3,8 @@ import { Prenda } from '../prenda';
 import { PrendaDetail } from './PrendaDetail';
 import { PrendaService } from '../Prenda.service';
 import { ActivatedRoute } from '@angular/router';
+import { Outfit } from 'src/app/outfit/outfit';
+import { OutfitService } from 'src/app/outfit/outfit.service';
 
 @Component({
   selector: 'app-prenda-detail',
@@ -13,7 +15,9 @@ export class PrendaDetailComponent implements OnInit {
 
   @Input() prendaDetail!: PrendaDetail;
 
-  constructor(private route: ActivatedRoute, private prendaService: PrendaService) { }
+  outfits: Outfit[] = [];
+
+  constructor(private route: ActivatedRoute, private prendaService: PrendaService, private outfitService: OutfitService) { }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
@@ -21,6 +25,13 @@ export class PrendaDetailComponent implements OnInit {
       this.prendaService.getPrendaById(+id).subscribe((prendaDetail) => {
         this.prendaDetail = prendaDetail;
       });
+      this.outfitService.getOutfits().subscribe((outfits: Outfit[]) => {
+        for (let i = 0; i < outfits.length; i++) {
+          if (+id in outfits[i].prendas) {
+            this.outfits.push(outfits[i]);
+          }
+        }
+      }
     }
   }
 }
