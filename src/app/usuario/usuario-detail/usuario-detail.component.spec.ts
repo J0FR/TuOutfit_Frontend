e7fe29adc183ee7ac777ce4429f805637fd76285
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2023-06-04 22:30:01 -0500

    t

--- src/app/usuario/usuario-detail/usuario-detail.component.spec.ts
+++ src/app/usuario/usuario-detail/usuario-detail.component.spec.ts
@@ -1,30 +1,80 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UsuarioDetailComponent } from './usuario-detail.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { UsuarioService } from '../usuario.service';
+import { UsuarioDetail } from '../usuario-detail';
 
 describe('UsuarioDetailComponent', () => {
   let component: UsuarioDetailComponent;
   let fixture: ComponentFixture<UsuarioDetailComponent>;
-  let debug: DebugElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [ RouterTestingModule, HttpClientTestingModule ],
-      declarations: [ UsuarioDetailComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UsuarioDetailComponent]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UsuarioDetailComponent);
     component = fixture.componentInstance;
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render the user name', () => {
+    const userName = 'John Doe';
+    const usuarioDetail: UsuarioDetail = {
+      id: 1,
+      nombre: userName,
+      genero: 'Male',
+      edad: 25,
+      email: 'john@example.com',
+      comentarios: [],
+      outfits_favoritos: []
+    };
+    component.usuarioDetail = usuarioDetail;
     fixture.detectChanges();
-    debug = fixture.debugElement;
+    const element = fixture.nativeElement.querySelector('p');
+    expect(element.textContent).toContain(userName);
+  });
+
+  it('should render the user gender and age', () => {
+    const userGender = 'Male';
+    const userAge = 25;
+    const usuarioDetail: UsuarioDetail = {
+      id: 1,
+      nombre: 'John Doe',
+      genero: userGender,
+      edad: userAge,
+      email: 'john@example.com',
+      comentarios: [],
+      outfits_favoritos: []
+    };
+    component.usuarioDetail = usuarioDetail;
+    fixture.detectChanges();
+    const genderElement = fixture.nativeElement.querySelector('.genero');
+    const ageElement = fixture.nativeElement.querySelector('.edad');
+    expect(genderElement.textContent).toContain(userGender);
+    expect(ageElement.textContent).toContain(userAge.toString());
+  });
+
+  it('should render the correct user name, gender, and age when usuarioDetail is provided', () => {
+    const usuarioDetail: UsuarioDetail = {
+      id: 1,
+      nombre: 'John Doe',
+      genero: 'Male',
+      edad: 25,
+      email: 'john@example.com',
+      comentarios: [],
+      outfits_favoritos: []
+    };
+    component.usuarioDetail = usuarioDetail;
+    fixture.detectChanges();
+    const element = fixture.nativeElement.querySelector('p');
+    const genderElement = fixture.nativeElement.querySelector('.genero');
+    const ageElement = fixture.nativeElement.querySelector('.edad');
+    expect(element.textContent).toContain(usuarioDetail.nombre);
+    expect(genderElement.textContent).toContain(usuarioDetail.genero);
+    expect(ageElement.textContent).toContain(usuarioDetail.edad.toString());
   });
 });
--- src/app/usuario/usuario-list/usuario-list.component.spec.ts
+++ src/app/usuario/usuario-list/usuario-list.component.spec.ts
@@ -1,41 +1,82 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-import { faker } from '@faker-js/faker';
+// usuario-list.component.spec.ts
 
+import { ComponentFixture, TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
 import { UsuarioListComponent } from './usuario-list.component';
 import { UsuarioService } from '../usuario.service';
 import { HttpClientModule } from '@angular/common/http';
-import { Usuario } from '../usuario';
 import { UsuarioDetail } from '../usuario-detail';
-
-// TODO: crear tests
+import { By } from '@angular/platform-browser';
 
 describe('UsuarioListComponent', () => {
   let component: UsuarioListComponent;
   let fixture: ComponentFixture<UsuarioListComponent>;
-  let debug: DebugElement;
+  let usuarioService: UsuarioService;
+  const testUsuarios: UsuarioDetail[] = [
+    new UsuarioDetail(1, 'Test User 1', 'male', 30, 'test1@example.com', [], []),
+    new UsuarioDetail(2, 'Test User 2', 'female', 25, 'test2@example.com', [], []),
+  ];
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports : [HttpClientModule],
-      declarations: [ UsuarioListComponent ],
-      providers: [UsuarioService]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      declarations: [UsuarioListComponent],
+      providers: [UsuarioService],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UsuarioListComponent);
     component = fixture.componentInstance;
+    usuarioService = TestBed.inject(UsuarioService);
 
-    let testUsuarios: Array <UsuarioDetail> = [];
-
+    spyOn(usuarioService, 'getUsuarios').and.returnValue(of(testUsuarios));
     fixture.detectChanges();
   });
 
-  /* it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
-  }); */
+  });
+
+  it('should call getUsuarios on init', () => {
+    expect(usuarioService.getUsuarios).toHaveBeenCalled();
+  });
+
+  it('should select a user when onSelected is called', () => {
+    let userDetail = new UsuarioDetail(1, 'Test User 1', 'male', 30, 'test1@example.com', [], []);
+    component.onSelected(userDetail);
+    expect(component.selected).toBeTrue();
+    expect(component.selectedUsuario).toBe(userDetail);
+  });
+
+    it('should update state on onSelected()', fakeAsync(() => {
+      let userDetail = new UsuarioDetail(1, 'Test User 1', 'male', 30, 'test1@example.com', [], []);
+      component.onSelected(userDetail);
+      expect(component.selected).toBeTrue();
+      expect(component.selectedUsuario).toBe(userDetail);
+    }));
+
+    it('should update usuarios after calling getUsuarios', () => {
+      component.getUsuarios();
+      expect(component.usuarios).toBe(testUsuarios);
+    });
+
+    it('should display user detail when a user is selected', () => {
+      let userDetail = new UsuarioDetail(1, 'Test User 1', 'male', 30, 'test1@example.com', [], []);
+      component.onSelected(userDetail);
+      fixture.detectChanges();
+
+      const userDetailElement = fixture.debugElement.query(By.css('app-usuario-detail'));
+      expect(userDetailElement).toBeTruthy();
+    });
+
+    it('should display user list after getUsuarios', () => {
+      component.getUsuarios();
+      fixture.detectChanges();
+
+      const userElements = fixture.debugElement.queryAll(By.css('.col'));
+      expect(userElements.length).toBe(2);
+    });
+
+
 });
